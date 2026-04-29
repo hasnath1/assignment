db76e1b50affd6ee6a1dc7ce66f54c1b165b5b0c
hasnath1 <hasnathabdullah111@gmail.com>
2026-04-29 20:05:14 +0600

    for mahmudullah

--- main.c
+++ main.c
@@ -1,39 +1,87 @@
+// #include <stdio.h>
+
+// typedef long double ld;
+
+// ld add(ld x, ld y)
+// {
+// 	return x + y;
+// };
+
+// ld substract(ld x, ld y)
+// {
+// 	return x - y;
+// };
+
+// ld multiply(ld x, ld y)
+// {
+// 	return x * y;
+// };
+
+// ld divide(ld x, ld y)
+// {
+// 	return x / y;
+// };
+
+// int main()
+// {
+// 	ld x, y;
+
+// 	printf("Enter two numbers : (space separated)\n ");
+
+// 	scanf("%Lf %Lf", &x, &y);
+
+// 	printf("%.3Lf + %.3Lf = %.3Lf\n", x, y, add(x, y));
+// 	printf("%.3Lf - %.3Lf = %.3Lf\n", x, y, substract(x, y));
+// 	printf("%.3Lf * %.3Lf = %.3Lf\n", x, y, multiply(x, y));
+// 	printf("%.3Lf / %.3Lf = %.3Lf\n", x, y, divide(x, y));
+
+// 	return 0;
+// }
+
+// Start -->
+
 #include <stdio.h>
 
-typedef long double ld;
-
-ld add(ld x, ld y)
+int add(int a, int b)
 {
-	return x + y;
-};
+	return a + b;
+}
 
-ld substract(ld x, ld y)
+int sub(int a, int b)
 {
-	return x - y;
-};
+	return a - b;
+}
 
-ld multiply(ld x, ld y)
+int multi(int a, int b)
 {
-	return x * y;
-};
+	return a * b;
+}
 
-ld divide(ld x, ld y)
+int divide(int a, int b)
 {
-	return x / y;
-};
+	return a / b;
+}
 
 int main()
 {
-	ld x, y;
 
-	printf("Enter two numbers : (space separated)\n ");
+	int x, y;
 
-	scanf("%Lf %Lf", &x, &y);
+	printf("Enter two numbers : \n");
 
-	printf("%.3Lf + %.3Lf = %.3Lf\n", x, y, add(x, y));
-	printf("%.3Lf - %.3Lf = %.3Lf\n", x, y, substract(x, y));
-	printf("%.3Lf * %.3Lf = %.3Lf\n", x, y, multiply(x, y));
-	printf("%.3Lf / %.3Lf = %.3Lf\n", x, y, divide(x, y));
+	scanf("%d %d", &x, &y);
+
+	printf("%d + %d = %d\n", x, y, add(x, y));
+	printf("%d - %d = %d\n", x, y, sub(x, y));
+	printf("%d * %d = %d\n", x, y, multi(x, y));
+	printf("%d / %d = %d\n", x, y, divide(x, y));
+
+	char c1, c2, c3, c4, c5,c6;
+
+	printf("Enter your name:(max 5 chars)\n");
+	scanf("%c%c%c%c%c%c", &c1, &c2, &c3, &c4, &c5,&c6);
+
+	printf("Hello %c%c%c%c%c%c!\n", c1, c2, c3, c4, c5,c6);
 
 	return 0;
 }
